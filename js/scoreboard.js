0275822165a59d2f5f9f70dd92f461aa806c6702
Kerry Buckley <kerryjbuckley@gmail.com>
2015-09-19 19:39:54 +0100

    Add continue button

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,19 +1,24 @@
-Scoreboard = (function() {
+var Scoreboard = (function() {
   var _this = {};
   var locked = false;
+
+  _this.init = function() {
+    hide_continue_button();
+  }
 
   _this.bind = function() {
     $(document).bind("keypress", Scoreboard.keypress);
     $(document).bind("buzz", Scoreboard.buzz);
     $(".plus").bind("click", Scoreboard.addPoint);
     $(".minus").bind("click", Scoreboard.subtractPoint);
+    $("#continue").bind("click", Scoreboard.unlock);
   };
 
   _this.keypress = function(event) {
     if (event.which >= 49 && event.which <= 52) {
       $(document).trigger("buzz", {player: event.which - 48});
     } else if (event.which == 32) {
-      unlock();
+      Scoreboard.unlock();
     }
   };
 
@@ -25,13 +30,27 @@ Scoreboard = (function() {
 
   _this.addPoint = function() {
     adjustScore($(this).data("index"), 1);
-    unlock();
+    Scoreboard.unlock();
   };
 
   _this.subtractPoint = function() {
     adjustScore($(this).data("index"), -1);
-    unlock();
+    Scoreboard.unlock();
   };
+
+  _this.unlock = function() {
+    locked = false;
+    $(".player").removeClass("disabled");
+    hide_continue_button();
+  };
+
+  function show_continue_button() {
+    $("#continue").show();
+  }
+
+  function hide_continue_button() {
+    $("#continue").hide();
+  }
 
   function adjustScore(player, amount) {
     score = $("#score-" + player);
@@ -42,13 +61,10 @@ Scoreboard = (function() {
     locked = true;
     $(".player").addClass("disabled");
     $("#player-" + player).removeClass("disabled");
+    show_continue_button();
   }
 
-  function unlock() {
-    locked = false;
-    $(".player").removeClass("disabled");
-  }
-
+  _this.init();
   return _this;
 }).call($);
 
