67f28821f029e29dcb531e9ea7a7a23fb0b6ea10
Kerry Buckley <kerryjbuckley@gmail.com>
2015-09-20 18:45:54 +0100

    Save names and scores in local storage

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -6,8 +6,10 @@ var Scoreboard = (function() {
   _this.init = function() {
     $.each([1, 2, 3, 4], function(_index, player) {
       $($.templates("#playerTemplate").render({player: player})).appendTo("#players");
+      $("#player-" + player + " .name").text(localStorage.getItem("name-" + player) || "Player " + player);
+      setScore(player, localStorage.getItem("score-" + player) || 0);
+      $("#player-" + player + " .name").editable({success: function(_response, value) { Scoreboard.saveName(player, value); }});
     });
-    $(".name").editable();
     hide_continue_button();
   };
 
@@ -15,6 +17,8 @@ var Scoreboard = (function() {
     $(document).bind("keypress", Scoreboard.keypress);
     $(document).bind("buzz", Scoreboard.buzz);
     $("#reset-button").bind("click", Scoreboard.reset);
+    $(".name").bind("shown", function() { locked = true; });
+    $(".name").bind("hidden", Scoreboard.unlock);
     $(".plus").bind("click", Scoreboard.addPoint);
     $(".minus").bind("click", Scoreboard.subtractPoint);
     $("#continue").bind("click", Scoreboard.unlock);
@@ -35,19 +39,27 @@ var Scoreboard = (function() {
     }
   };
 
+  _this.saveName = function(player, name) {
+    localStorage.setItem("name-" + player, name);
+  };
+
   _this.reset = function() {
-    $(".score").text(0);
+    $.each([1, 2, 3, 4], function(_index, player) {
+      setScore(player, 0);
+    });
   };
 
   _this.addPoint = function() {
     this.blur();
-    adjustScore($(this).data("index"), 1);
+    player = $(this).data("index");
+    setScore(player, currentScore(player) + 1);
     Scoreboard.unlock();
   };
 
   _this.subtractPoint = function() {
     this.blur();
-    adjustScore($(this).data("index"), -1);
+    player = $(this).data("index");
+    setScore(player, currentScore(player) - 1);
     Scoreboard.unlock();
   };
 
@@ -65,9 +77,17 @@ var Scoreboard = (function() {
     $("#continue").hide();
   }
 
-  function adjustScore(player, amount) {
-    score = $("#score-" + player);
-    score.text(parseInt(score.text()) + amount);
+  function currentScore(player) {
+    return parseInt(scoreElement(player).text());
+  }
+
+  function setScore(player, score) {
+    scoreElement(player).text(score);
+    localStorage.setItem("score-" + player, score);
+  }
+
+  function scoreElement(player) {
+    return $("#score-" + player);
   }
 
   function lock(player) {
